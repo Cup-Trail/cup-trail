e55aaff64eabf7b603d9982f46128f4f1b1e3ec2
Katherine Wong <katmwong29@gmail.com>
2025-07-10 15:59:52 -0700

    put navigator at the root

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
-// App.js
-import React from 'react';
-import AppNavigator from './navigation/AppNavigator';
+// react imports
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// screens
+import SearchScreen from './screens/searchScreen';
+
+const Stack = createNativeStackNavigator();
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={SearchScreen} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
 }
