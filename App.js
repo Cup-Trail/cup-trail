2700edef24a67bf1c47498f6063939c9fa28c10f
Katherine Wong <katmwong29@gmail.com>
2025-07-10 18:25:56 -0700

    combine insertShop functionality into search bar

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 // react imports
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // screens
 import SearchScreen from './screens/searchScreen';
+import InsertShop from './screens/insertShopScreen';
 
 const Stack = createNativeStackNavigator();
 function RootStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={SearchScreen} />
+      <Stack.Screen name="InsertShop" component={InsertShop} />
     </Stack.Navigator>
   );
 }
