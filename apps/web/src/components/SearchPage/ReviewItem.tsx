bf9e9e34bb31d30986e8e00dc233a7d38759efda
John Adams <78485956+jpaoad31@users.noreply.github.com>
2025-12-26 23:15:14 -0800

    navigate using `useNavigate`

--- apps/web/src/components/SearchPage/ReviewItem.tsx
+++ apps/web/src/components/SearchPage/ReviewItem.tsx
@@ -1,5 +1,6 @@
-import { ReviewRow } from '@cuptrail/core';
+import type { LocationState, ReviewRow } from '@cuptrail/core';
 import { Box, Typography, Link } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 import { renderStars } from '../../utils';
 
@@ -8,10 +9,18 @@ interface ReviewItemProps {
 }
 
 const ReviewItem = ({ item }: ReviewItemProps) => {
+  const navigate = useNavigate();
   const shopName = item.shop_drinks.shops?.name;
   const drinkName = item.shop_drinks.drinks?.name;
   const title = drinkName ? `${drinkName} @ ${shopName}` : "Review";
   const reviewDate = new Date(item.created_at).toLocaleDateString();
+  const navigateToShop = () => navigate(`/shop/${item.shop_drinks.shops.id}`, {
+    state: {
+      shopName,
+      address: item.shop_drinks.shops.address,
+      shopId: item.shop_drinks.shops.id,
+    } as LocationState,
+  })
   return (
     <Box
       key={item.id}
@@ -21,7 +30,7 @@ const ReviewItem = ({ item }: ReviewItemProps) => {
       flexDirection="column"
       gap={2}
     >
-      <Typography fontWeight={600}><Link color="textPrimary" underline="hover" href={`/shop/${item.shop_drinks.shops.id}`}>{title}</Link></Typography>
+      <Typography fontWeight={600}><Link color="textPrimary" underline="hover" onClick={navigateToShop} >{title}</Link></Typography>
       
       <Box color="warning.main" fontSize="20px">
         {renderStars(item.rating)}
