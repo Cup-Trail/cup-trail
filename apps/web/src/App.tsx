5af0417239e9f247dcc71bc8602e1dfae565c58b
Katherine Wong <katmwong29@gmail.com>
2026-01-05 22:06:42 -0800

    keep styles in web for now

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,3 @@
-import '@cuptrail/ui/styles/style.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
