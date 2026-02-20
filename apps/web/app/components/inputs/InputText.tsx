47aaf5e11f44bfd9637947d5e10ef02cf6395809
John Adams <78485956+jpaoad31@users.noreply.github.com>
2026-02-19 22:33:14 -0800

    InputText element

---
+++ apps/web/app/components/inputs/InputText.tsx
@@ -0,0 +1,24 @@
+import React, {} from 'react';
+import { cn } from '@utils/ui';
+
+type InputTextProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
+  label?: string,
+  containerClassName?: string,
+  labelClassName?: string,
+  name: string,
+}
+
+export default function InputText({label, containerClassName, labelClassName, className, ...props}: InputTextProps) {
+
+  return (
+    <div className={cn('flex flex-col gap-2 items-start', containerClassName)}>
+      {!!label && <label htmlFor={props.name} className={labelClassName}>{label}</label>}
+      <input
+        type='text'
+        className={cn('py-2 px-3 bg-surface-2 border-border-default border rounded-lg w-full xs:w-72', className)}
+        id={props.name}
+        {...props}
+      />
+    </div>
+  )
+}
